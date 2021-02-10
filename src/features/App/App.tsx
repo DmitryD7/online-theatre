8b9bb954b49dc58b78dced22d82e3fd1fa89b45f
DmitryDemid <dimon.demid15@gmail.com>
2021-02-09 23:33:39 -0500

    Making nav bar

--- src/features/App/App.tsx
+++ src/features/App/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import s from './App.module.scss'
 import {MoviesRows} from "../MoviesRow/MoviesRows";
 import {Header} from "../Header/Header";
+import {Nav} from "../Nav/Nav";
 
 export const BASE_IMG_URL = 'https://image.tmdb.org/t/p/original/'
 
 function App() {
 
     return <div className={s.App}>
+        <Nav/>
         <Header/>
         <MoviesRows/>
     </div>
--- src/features/Nav/Nav.tsx
+++ src/features/Nav/Nav.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import s from "./Nav.module.scss"
+import theatreLogo from "../../assets/images/movieTheatre.png"
+import userAvatar from "../../assets/images/avatar.png"
 
 export const Nav = () => {
-    return <div className={s.nav}>
+    const [isSeen, setIsSeen] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => window.scrollY > 100 ? setIsSeen(true) : setIsSeen(false))
+        return () => window.removeEventListener('scroll', () => window.scrollY > 100 ? setIsSeen(true) : setIsSeen(false))
+    }, [])
+
+    return <div className={`${s.nav} ${isSeen ? s.nav_bg_black : ''}`}>
+        <img src={theatreLogo} alt="logo" className={s.nav_logo}/>
+        <img src={userAvatar} alt="avatar" className={s.nav_avatar}/>
     </div>
 }
